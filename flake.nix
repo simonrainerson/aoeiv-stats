a5b76dd3c83c579264b35571f07f08aeb603072b
Simon Rainerson <simonr88@gmail.com>
2025-03-21 01:09:25 +0100

    🎉 Initial commit
    Setup the nix file, currently only builds with sandobx off. And use the
    button example for source.

---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  description = "Keeping track of AOEIV stats";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "aoeiv-stats";
+          nativeBuildInputs = with pkgs.elmPackages; [
+            elm
+            elm-format
+            elm-optimize-level-2
+            elm-test
+          ];
+
+          src = ./.;
+
+          configurePhase = ''
+            export HOME=$PWD/.home
+          '';
+
+          buildPhase = ''
+            elm make src/Main.elm
+          '';
+
+          checkPhase = ''
+            echo -e "\033[1;33mChecking formatting...\033[0m"
+            elm-format --validate src
+            echo -e "\033[1;32mFormat OK!\033[0m"
+
+            echo -e "Runnings tests..."
+            elm-test
+          '';
+          doCheck = true;
+
+          installPhase = ''
+            mkdir -p $out
+            cp index.html $out/index.html
+          '';
+        };
+        devShells.default = pkgs.mkShell {
+          name = "aoeiv-stats";
+
+          packages = with pkgs.elmPackages; [
+            elm
+            elm-doc-preview
+            elm-format
+            elm-optimize-level-2
+            elm-test
+            elm-language-server
+            #pkgs.caddy
+            #pkgs.nodejs_20
+            #pkgs.nodePackages.terser
+            #pkgs.shellcheck
+          ];
+
+          # shellHook = ''
+          #   export project="$PWD"
+          #   export build="$project/.build"
+          #   export PATH="$project/bin:$PATH"
+          #
+          #   npm install --loglevel silent
+          # '';
+        };
+      }
+    );
+}
